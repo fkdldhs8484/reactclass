1d7f0274e521b37dcbe9d3d1481b4fe7c29ff9e5
fkdldhs8484 <fkdldhs8484@gmail.com>
2022-11-08 11:50:14 +0900

    11:50

---
+++ vue_api/src/views/MovieView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <HeaderCont />
+    <TitleCont />
+    <ContantCont />
+    <FooterCont />
+  </div>
+</template>
+
+<script>
+import HeaderCont from "@/components/HeaderCont.vue";
+import FooterCont from "@/components/FooterCont.vue";
+import TitleCont from "@/components/TitleCont.vue";
+import ContantCont from "@/components/ContantCont.vue";
+
+export default {
+  components: {
+    HeaderCont,
+    FooterCont,
+    TitleCont,
+    ContantCont,
+  },
+};
+</script>
