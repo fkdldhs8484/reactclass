fba3dd9a5ccb9776286277cb3a0650756b58d950
fkdldhs8484 <fkdldhs8484@gmail.com>
2022-11-10 14:17:52 +0900

    11.10

--- vue_api/src/views/MovieView.vue
+++ vue_api/src/views/MovieView.vue
@@ -1,7 +1,77 @@
 <template>
   <div>
     <HeaderCont />
-    <TitleCont />
+    <TitleCont name1="movie" name2="reference api" />
+    <section className="cont__refer">
+      <div className="container">
+        <div className="movie__inner">
+          <!-- movie__slider -->
+          <div class="movie__slider">
+            <swiper
+              :effect="'coverflow'"
+              :grabCursor="true"
+              :centeredSlides="true"
+              :slidesPerView="'auto'"
+              :initialSlide="3"
+              :coverflowEffect="{
+                rotate: 50,
+                stretch: 0,
+                depth: 100,
+                modifier: 1,
+                slideShadows: true,
+              }"
+              :pagination="true"
+              :modules="modules"
+              class="mySwiper"
+            >
+              <swiper-slide v-for="slider in sliders" :key="slider.id">
+                <ul>
+                  <li>
+                    <a :href="`https://image.tmdb.org/movie/${slider.id}`">
+                      <img
+                        :src="`https://image.tmdb.org/t/p/w500/${slider.poster_path}`"
+                      />
+                    </a>
+                  </li>
+                </ul>
+              </swiper-slide>
+            </swiper>
+          </div>
+          <!-- movie__search -->
+          <div class="movie__search">
+            <div class="container">
+              <h2>search</h2>
+              <form @submit.prevent="SearchMovies()">
+                <input
+                  id="search"
+                  type="search"
+                  placeholder="검색어를 입력하세요!"
+                  v-model="search"
+                />
+                <button type="submit">검색</button>
+              </form>
+            </div>
+          </div>
+          <!-- movie__movies" -->
+          <div class="movie__movies">
+            <div class="container">
+              <div class="movie__list">
+                <ul>
+                  <li v-for="movie in movies" :key="movie.id">
+                    <a href="">
+                      <img
+                        :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
+                        :alt="movie.title"
+                      />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
     <ContantCont />
     <FooterCont />
   </div>
@@ -12,6 +82,13 @@ import HeaderCont from "@/components/HeaderCont.vue";
 import FooterCont from "@/components/FooterCont.vue";
 import TitleCont from "@/components/TitleCont.vue";
 import ContantCont from "@/components/ContantCont.vue";
+import { ref } from "vue";
+
+import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import { EffectCoverflow, Pagination } from "swiper";
 
 export default {
   components: {
@@ -19,17 +96,119 @@ export default {
     FooterCont,
     TitleCont,
     ContantCont,
+    Swiper,
+    SwiperSlide,
   },
 
-  data: function () {
+  setup() {
+    const movies = ref([]);
+    const sliders = ref([]);
+    const search = ref("marvel");
+
+    const SearchMovies = async () => {
+      await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=448d3fe1610ebe02d04b6d72a4a8fd91&query=${search.value}`
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          movies.value = result.results;
+          search.value = "";
+        })
+        .catch((error) => console.log(error));
+    };
+    SearchMovies();
+
+    const TopMovies = async () => {
+      await fetch(
+        `https://api.themoviedb.org/3/movie/popular?api_key=448d3fe1610ebe02d04b6d72a4a8fd91&`
+      )
+        .then((response) => response.json())
+        .then((result) => (sliders.value = result.results))
+        .catch((error) => console.log(error));
+    };
+    TopMovies();
+
     return {
-      titles: [
-        { text: "we provide" },
-        { text: "visual coding" },
-        { text: "solutions" },
-        { text: "for you API" },
-      ],
+      movies,
+      sliders,
+      search,
+      TopMovies,
+      SearchMovies,
+      modules: [EffectCoverflow, Pagination],
     };
   },
 };
 </script>
+
+<style lang="scss">
+.movie__list {
+  ul {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    li {
+      width: 19%;
+      margin-bottom: 2%;
+    }
+  }
+}
+
+// movieSearch
+
+.movie__search {
+  margin-bottom: 100px;
+
+  .container {
+    position: relative;
+  }
+
+  h2 {
+    color: var(--black);
+    font-size: 40px;
+    text-indent: -9999px;
+    height: 0;
+  }
+  input {
+    background: #eee;
+    border: 1px solid #d0d0d0;
+    border-radius: 50px;
+    color: var(--black);
+    width: 100%;
+    padding: 14px 30px;
+    font-family: var(--font-kor);
+    margin-top: 20px;
+  }
+  button {
+    position: absolute;
+    right: 6px;
+    top: 5px;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    border: 0;
+    cursor: pointer;
+    z-index: 1000;
+    margin-top: 20px;
+    background: var(--white);
+  }
+}
+
+// swiper
+.swiper {
+  width: 100%;
+  padding-top: 50px;
+  padding-bottom: 190px;
+}
+
+.swiper-slide {
+  background-position: center;
+  background-size: cover;
+  width: 300px;
+  height: 300px;
+}
+
+.swiper-slide img {
+  display: block;
+  width: 100%;
+}
+</style>
--- vue_api/src/views/ReferView.vue
+++ vue_api/src/views/ReferView.vue
@@ -1,7 +1,23 @@
 <template>
   <div>
     <HeaderCont />
-    <TitleCont />
+    <TitleCont name1="reference" name2="api" />
+    <section className="cont__refer">
+      <div className="container">
+        <div className="refer__inner">
+          <h2>CSS</h2>
+          <ul className="refer__list">
+            <li v-for="refer in refers" :key="refer.title">
+              <a href="/">
+                <span>{{ refer.title }}</span>
+                <span>{{ refer.desc }}</span>
+                <span>{{ refer.star }}</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
     <ContantCont />
     <FooterCont />
   </div>
@@ -12,6 +28,7 @@ import HeaderCont from "@/components/HeaderCont.vue";
 import FooterCont from "@/components/FooterCont.vue";
 import TitleCont from "@/components/TitleCont.vue";
 import ContantCont from "@/components/ContantCont.vue";
+import { ref } from "vue";
 
 export default {
   components: {
@@ -20,5 +37,73 @@ export default {
     TitleCont,
     ContantCont,
   },
+
+  setup() {
+    const refers = ref([]);
+    const references = () => {
+      fetch(
+        "https://webstoryboy.github.io/react2022/src/assets/json/refer.json"
+      )
+        .then((response) => response.json())
+        .then((result) => (refers.value = result.data.htmlRefer))
+        .then((result) => console.log(result))
+        .catch((error) => console.log("error", error));
+    };
+    references();
+    return {
+      refers,
+      references,
+    };
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.refer__inner {
+  padding-bottom: 200px;
+  font-family: "NanumBarunpen";
+
+  h2 {
+    font-style: 30px;
+    color: var(--black);
+  }
+}
+
+.refer__list {
+  border: 1px solid var(--bg-dark-border);
+
+  li {
+    border-bottom: 1px solid var(--bg-dark-border);
+    a {
+      display: flex;
+      align-items: center;
+      width: 100%;
+      color: var(--black);
+
+      span {
+        display: inline-block;
+        padding: 15px 20px;
+      }
+
+      .num {
+        flex: 1 1 5%;
+        text-align: center;
+        border-right: 1px solid var(--bg-dark-border);
+      }
+
+      .name {
+        flex: 1 1 20%;
+        border-right: 1px solid var(--bg-dark-border);
+      }
+      .desc {
+        flex: 1 1 65%;
+        border-right: 1px solid var(--bg-dark-border);
+      }
+      .star {
+        flex: 1 1 10%;
+        text-align: center;
+      }
+    }
+  }
+}
+</style>
