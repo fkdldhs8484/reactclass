355417a651b1a8e5bb0661adf75814efeb2938d6
이유나 <109262091+fkdldhs8484@users.noreply.github.com>
2022-11-14 23:08:18 +0900

    1111

--- react_api/src/components/include/UnsplashSlider.jsx
+++ react_api/src/components/include/UnsplashSlider.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
+import "swiper/swiper.min.css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 import SwiperCore, { Autoplay, EffectCoverflow, Pagination } from "swiper";
--- react_api/src/components/include/UnsplashTag.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UnsplashTag = () => {
-  return <div>UnsplashTag</div>;
-};
-
-export default UnsplashTag;
