541a3fff892b9f465be0c982596c82cc2766143f
fkdldhs8484 <fkdldhs8484@gmail.com>
2022-11-15 10:28:23 +0900

    ddd

--- react_api/src/components/pages/Unsplash.jsx
+++ react_api/src/components/pages/Unsplash.jsx
@@ -11,49 +11,49 @@ import UnsplashBtn from "../include/UnsplashBtn";
 import Contact from "../layout/Contact";
 
 const Unsplash = () => {
-    const [images, setImages] = useState([]);
-    const [random, setRandom] = useState([]);
+  const [images, setImages] = useState([]);
+  const [random, setRandom] = useState([]);
 
-    const search = async (query) => {
-        await fetch(
-            `https://api.unsplash.com/photos?client_id=brs7PjWiLCgs71PZNkng8o9cZsgWxwyvUpc45Yjs5WY&query=marvel&page=1&query=${query}$per_page=30`
-        )
-            .then((response) => response.json())
-            .then((result) => console.log(result.results))
-            .catch((error) => console.log(error));
-    };
+  const search = async (query) => {
+    await fetch(
+      `https://api.unsplash.com/photos?client_id=brs7PjWiLCgs71PZNkng8o9cZsgWxwyvUpc45Yjs5WY&query=marvel&page=1&query=${query}$per_page=30`
+    )
+      .then((response) => response.json())
+      .then((result) => console.log(result.results))
+      .catch((error) => console.log(error));
+  };
 
-    useEffect(() => {
-        fetch(
-            "https://api.unsplash.com/photos?client_id=brs7PjWiLCgs71PZNkng8o9cZsgWxwyvUpc45Yjs5WY&query=seoul&conut=30"
-        )
-            .then((response) => response.json())
-            .then((result) => setImages(result))
-            .catch((error) => console.log(error));
-    }, []);
+  useEffect(() => {
+    fetch(
+      "https://api.unsplash.com/photos?client_id=brs7PjWiLCgs71PZNkng8o9cZsgWxwyvUpc45Yjs5WY&query=seoul&conut=30"
+    )
+      .then((response) => response.json())
+      .then((result) => setImages(result))
+      .catch((error) => console.log(error));
+  }, []);
 
-    useEffect(() => {
-        fetch(
-            "https://api.unsplash.com/photos?client_id=brs7PjWiLCgs71PZNkng8o9cZsgWxwyvUpc45Yjs5WY&conut=10"
-        )
-            .then((response) => response.json())
-            .then((result) => setRandom(result))
-            .catch((error) => console.log(error));
-    }, []);
+  useEffect(() => {
+    fetch(
+      "https://api.unsplash.com/photos?client_id=brs7PjWiLCgs71PZNkng8o9cZsgWxwyvUpc45Yjs5WY&conut=10"
+    )
+      .then((response) => response.json())
+      .then((result) => setRandom(result))
+      .catch((error) => console.log(error));
+  }, []);
 
-    return (
-        <>
-            <Header />
-            <Contents>
-                <Title title={["unsplash", "referece api"]} />
-                <UnsplashSlider random={random} />
-                <UnsplashSearch onSearch={search} />
-                <UnsplashBtn onSearch={search} />
-                <UnsplashCont images={images} />
-                <Contact />
-            </Contents>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Contents>
+        <Title title={["unsplash", "referece api"]} />
+        <UnsplashSlider random={random} />
+        <UnsplashSearch onSearch={search} />
+        <UnsplashBtn onSearch={search} />
+        <UnsplashCont images={images} />
+        <Contact />
+      </Contents>
+      <Footer />
+    </>
+  );
 };
 export default Unsplash;
