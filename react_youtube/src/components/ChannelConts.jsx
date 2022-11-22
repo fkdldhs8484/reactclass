9237278eabac956c07b17a1126def0bed893a5e5
fkdldhs8484 <fkdldhs8484@gmail.com>
2022-11-22 14:06:12 +0900

    1

--- react_youtube/src/components/ChannelConts.jsx
+++ react_youtube/src/components/ChannelConts.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { fetchAPI } from '../utils/fetchAPI'
+import { useParams } from 'react-router-dom'
+
+import { Videos } from './'
 
 const ChannelConts = () => {
-  return <div>ChannelConts</div>
+  const [channelDetail, setChannelDetail] = useState()
+  const [videos, setVideos] = useState(null)
+  const { id } = useParams()
+
+  useEffect(() => {
+    const fetchResults = async () => {
+      const data = await fetchAPI(`channels?part=snippet&id=${id}`)
+
+      setChannelDetail(data?.items[0])
+
+      const videosData = await fetchAPI(
+        `search?channelId=${id}&part=snippet&order=date`
+      )
+      setVideos(videosData?.items)
+    }
+    fetchResults()
+  }, [id])
+
+  return (
+    <section id="channelConts">
+      <div
+        className="channel-header"
+        style={{
+          backgroundImage: `url(${channelDetail?.brandingSettings?.image?.bannerExternalUrl})`,
+        }}
+      ></div>
+      <div className="channel-infor">
+        <img src={channelDetail?.snippet?.thumbnails?.medium?.url} alt="d" />
+        <h3>{channelDetail?.snippet?.title}</h3>
+        <div>
+          <span>구독자수 : {channelDetail?.statistics?.subscriberCount}</span>
+          <br />
+          <span>총 비디오 갯수 : {channelDetail?.statistics?.videoCount}</span>
+          <br />
+          <span>비디오 카운트 : {channelDetail?.statistics?.viewCount}</span>
+          <br />
+        </div>
+      </div>
+      <div className="channel-videos">
+        <Videos videos={videos} />
+      </div>
+    </section>
+  )
 }
 
 export default ChannelConts
--- react_youtube/src/components/VideoConts.jsx
+++ react_youtube/src/components/VideoConts.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react'
+import { AiFillHeart } from 'react-icons/ai'
 import ReactPlayer from 'react-player'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import { fetchAPI } from '../utils/fetchAPI'
+
+import { Videos, Loader } from './'
 
 const VideoConts = () => {
   const [videoDetail, setVideoDetail] = useState(null)
@@ -16,24 +19,40 @@ const VideoConts = () => {
     )
   }, [id])
 
-  // const {
-  //   snippet: { title, channelId, channelTitle },
-  //   statistics: { viewCount, likeCount },
-  // } = videoDetail
+  // 로딩소스 (꼭 넣어야 타이틀등이 나옴)
+  if (!videoDetail?.snippet) return <Loader />
+
+  const {
+    snippet: { title, channelId, channelTitle },
+    statistics: { viewCount, likeCount },
+  } = videoDetail
 
   return (
     <section className="videoconts">
       <div className="container">
         <div className="left">
           <div className="play">
-            <ReactPlayer
-              url={`https://www.youtube.com/watch?v=${id}`}
-              controls
-            />
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} />
           </div>
-          <div className="desc"></div>
+          <div className="desc">
+            <span className="title">{title}</span>
+            <div className="channel">
+              <Link to={`/channel/${channelId}`}>{channelTitle}</Link>
+            </div>
+            <div className="count">
+              <span className="view">
+                조회수 {videoDetail.statistics?.viewCount}회
+              </span>
+              <span className="like">
+                <AiFillHeart />
+                {videoDetail.statistics?.likeCount}
+              </span>
+            </div>
+          </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <Videos videos={videos} layout="column" />
+        </div>
       </div>
     </section>
   )
