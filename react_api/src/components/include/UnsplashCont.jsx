ba0987b39c983b68293a3da6a644947e1e32ec5f
fkdldhs8484 <fkdldhs8484@gmail.com>
2022-11-30 14:28:57 +0900

    1130

--- react_api/src/components/include/UnsplashCont.jsx
+++ react_api/src/components/include/UnsplashCont.jsx
@@ -1,31 +1,28 @@
 import React from "react";
 
 const UnsplashItem = ({ image }) => {
-    return (
-        <li>
-            <a href={`https://unsplash.com/photos/${image.id}`}>
-                <img
-                    src={image.urls.regular}
-                    alt={image.urls.alt_description}
-                />
-            </a>
-        </li>
-    );
+  return (
+    <li>
+      <a href={`https://unsplash.com/photos/${image.id}`}>
+        <img src={image.urls.regular} alt={image.urls.alt_description} />
+      </a>
+    </li>
+  );
 };
 
 const UnsplashCont = ({ images }) => {
-    return (
-        <section className="cont__unsplash">
-            <div className="container">
-                <div className="unsplash__inner">
-                    <ul>
-                        {images.map((image, index) => (
-                            <UnsplashItem key={index} image={image} />
-                        ))}
-                    </ul>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section className="cont__unsplash">
+      <div className="container">
+        <div className="unsplash__inner">
+          <ul>
+            {images.map((image, index) => (
+              <UnsplashItem key={index} image={image} />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default UnsplashCont;
